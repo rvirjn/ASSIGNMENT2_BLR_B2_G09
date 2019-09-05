9837d18028eb2636affea57c0321e60001fe5891
Kaviyarasu G <kaviyarg@amazon.com>
2019-09-05 19:48:03 +0530

    updated the logic to use dynamic programming logic

--- AS2_PS4_AP_G09.py
+++ AS2_PS4_AP_G09.py
@@ -1,10 +1,6 @@
 # Description: Data Structure Assignment-2
 # ASSIGNMENT2_BLR_B2_G09
 
-topics = ['DM', 'NLP', 'AI', 'SDA', 'IP',
-          'BD', 'GM', 'ML', 'EC',
-          'WMC', 'CC']
-
 
 def student_with_pref(input='inputPS4.txt'):
     """
@@ -12,30 +8,38 @@ def student_with_pref(input='inputPS4.txt'):
     :param input: input file
     :return: dict key= student , value= [their preferences]
     """
-    students = {}
+    studentsPref = []
     with open(input) as f:
         for line in f.readlines():
             line = line.strip()
             values = line.split('/')
-            student = values[0]
-            students[student] = values[1:]
-        # print students
-    return students
+            studentsPref.append(map(str.strip, values[1:]))
+        #End of For
+    #End of file
+    return studentsPref
 
 
-def unique_allocation(student_with_pref):
+def unique_allocation(student_with_pref, studentIndex, electedTopics):
     """
 
     :param student_with_pref: eg {stduent: [their preference]}
     :return: Number of allocations
     """
-    # print student_with_pref
-    for student in student_with_pref.keys():
-        preferences = student_with_pref.get(student)
-        # print '%s preferences %s' % (student, preferences)
-        # :TODO add Logic for allocations combinations
-    output = 'The total number of allocations possible is: %s'
-    return output % 7588
+    if studentIndex >= len(student_with_pref):
+        print(electedTopics)
+        return 1
+    # end of If
+
+    returnVal = 0
+
+    for t in student_with_pref[studentIndex]:
+        if t not in electedTopics:
+            local_topics = electedTopics[:]
+            local_topics.append(t)
+            returnVal += unique_allocation(student_with_pref, studentIndex + 1, local_topics[:])
+        # End of If
+    # End of For
+    return returnVal
 
 
 def writeTofile(output, file="outputPS4.txt"):
@@ -47,6 +51,7 @@ def writeTofile(output, file="outputPS4.txt"):
 if __name__ == "__main__":
     open("outputPS4.txt", "w").close()
     st_with_pref = student_with_pref()
-    output = unique_allocation(st_with_pref)
-    print output
-    writeTofile(output)
+    data = unique_allocation(st_with_pref, 0, [])
+    output = 'The total number of allocations possible is: %s'
+    print(output % data)
+    writeTofile(output % data)
