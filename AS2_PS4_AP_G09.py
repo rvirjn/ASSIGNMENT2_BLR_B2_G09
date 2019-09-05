6186f522908ed2bb1ea94f39e7be39dd9855cf21
Kaviyarasu G <kaviyarg@amazon.com>
2019-09-05 20:17:58 +0530

    Updated the variable names and inline comments

--- AS2_PS4_AP_G09.py
+++ AS2_PS4_AP_G09.py
@@ -2,11 +2,12 @@
 # ASSIGNMENT2_BLR_B2_G09
 
 
-def student_with_pref(input='inputPS4.txt'):
+def read_students_pref(input='inputPS4.txt'):
     """
-    Function returns students with their preferences
+    Function returns the student preferences in the input students' order.
+
     :param input: input file
-    :return: dict key= student , value= [their preferences]
+    :return: dict value= [array of preferences]
     """
     studentsPref = []
     with open(input) as f:
@@ -19,39 +20,48 @@ def student_with_pref(input='inputPS4.txt'):
     return studentsPref
 
 
-def unique_allocation(student_with_pref, studentIndex, electedTopics):
+def unique_allocation(students_pref, student_index, elected_topics):
     """
+    Finds the number of possible allocations possible from the current student's position till the last student
 
-    :param student_with_pref: eg {stduent: [their preference]}
+    :param students_pref: eg [[s1 pref],[s2 pref],...]
+    :param student_index: Current student's index for whom we should elect a suitable elective.
+    :param elected_topics: List of topics which are chosen by previous students
     :return: Number of allocations
     """
-    if studentIndex >= len(student_with_pref):
-        print(electedTopics)
+    if student_index >= len(students_pref):
         return 1
     # end of If
 
-    returnVal = 0
+    return_val = 0
 
-    for t in student_with_pref[studentIndex]:
-        if t not in electedTopics:
-            local_topics = electedTopics[:]
+    for t in students_pref[student_index]:
+        if t not in elected_topics:
+            local_topics = elected_topics[:]
             local_topics.append(t)
-            returnVal += unique_allocation(student_with_pref, studentIndex + 1, local_topics[:])
+            return_val += unique_allocation(students_pref, student_index + 1, local_topics)
         # End of If
     # End of For
-    return returnVal
+    return return_val
 
 
-def writeTofile(output, file="outputPS4.txt"):
+def write_to_file(data, file="outputPS4.txt"):
+    """
+    writes the provided output data into the provided file
+
+    :param data: data to be written
+    :param file: file path/name to be written
+    :return:
+    """
     f = open(file, "a")
-    f.write(output)
+    f.write(data)
     f.close()
 
 
 if __name__ == "__main__":
     open("outputPS4.txt", "w").close()
-    st_with_pref = student_with_pref()
-    data = unique_allocation(st_with_pref, 0, [])
+    st_with_pref = read_students_pref()
+    count = unique_allocation(st_with_pref, 0, [])
     output = 'The total number of allocations possible is: %s'
-    print(output % data)
-    writeTofile(output % data)
+    #print(output % count)
+    write_to_file(output % count)
